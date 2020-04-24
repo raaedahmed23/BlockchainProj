42b3681703ec9c1eeda333f560c01e66794ab7dc
AishwwaryaM <58990857+AishwwaryaM@users.noreply.github.com>
2020-04-24 20:04:57 +0530

    Update HomePage.java

--- gui/HomePage.java
+++ gui/HomePage.java
@@ -1,4 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package cryptoproj;
+
+/**
+ *
+ * @author SRENIVASS
+ */
 public class HomePage extends javax.swing.JFrame {
 
     /**
@@ -7,7 +17,10 @@ public class HomePage extends javax.swing.JFrame {
     public HomePage() {
         initComponents();
     }
-
+    public HomePage(NoobChain n) {
+        initComponents();
+        nc=n;
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -19,31 +32,23 @@ public class HomePage extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        EvidenceArea = new javax.swing.JTextArea();
         MineBlockButton = new javax.swing.JButton();
         AddEvidence = new javax.swing.JButton();
         viewUserButton = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        SuspectNameField = new javax.swing.JTextField();
-        VictimNameField = new javax.swing.JTextField();
+        output = new javax.swing.JLabel();
+        SubmissionLoc = new javax.swing.JTextField();
+        evi_num = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        evi_info = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(null);
 
-        jLabel3.setText("Suspect Name  :");
+        jLabel3.setText("Submission Location  :");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(120, 50, 90, 30);
-
-        EvidenceArea.setColumns(20);
-        EvidenceArea.setRows(5);
-        jScrollPane1.setViewportView(EvidenceArea);
-
-        jPanel1.add(jScrollPane1);
-        jScrollPane1.setBounds(250, 190, 330, 130);
+        jLabel3.setBounds(90, 50, 120, 30);
 
         MineBlockButton.setText("MINE BLOCK");
         MineBlockButton.addActionListener(new java.awt.event.ActionListener() {
@@ -61,7 +66,7 @@ public class HomePage extends javax.swing.JFrame {
             }
         });
         jPanel1.add(AddEvidence);
-        AddEvidence.setBounds(320, 340, 160, 50);
+        AddEvidence.setBounds(330, 290, 160, 50);
 
         viewUserButton.setText("VIEW USER");
         viewUserButton.addActionListener(new java.awt.event.ActionListener() {
@@ -72,27 +77,41 @@ public class HomePage extends javax.swing.JFrame {
         jPanel1.add(viewUserButton);
         viewUserButton.setBounds(400, 460, 120, 50);
 
-        jLabel2.setText("ALL DETAILS ABOVE ARE MANDATORY TO ADD EVIDENCE");
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(250, 400, 300, 30);
-        jPanel1.add(SuspectNameField);
-        SuspectNameField.setBounds(240, 50, 340, 40);
+        output.setText("ALL DETAILS ABOVE ARE MANDATORY TO ADD EVIDENCE");
+        jPanel1.add(output);
+        output.setBounds(270, 360, 300, 30);
 
-        VictimNameField.addActionListener(new java.awt.event.ActionListener() {
+        SubmissionLoc.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                VictimNameFieldActionPerformed(evt);
+                SubmissionLocActionPerformed(evt);
             }
         });
-        jPanel1.add(VictimNameField);
-        VictimNameField.setBounds(240, 110, 340, 40);
+        jPanel1.add(SubmissionLoc);
+        SubmissionLoc.setBounds(240, 50, 340, 40);
 
-        jLabel1.setText("Victim Name    :");
+        evi_num.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                evi_numActionPerformed(evt);
+            }
+        });
+        jPanel1.add(evi_num);
+        evi_num.setBounds(240, 110, 340, 40);
+
+        jLabel1.setText("Evidence Number   :");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(120, 110, 90, 40);
+        jLabel1.setBounds(90, 110, 120, 40);
 
-        jLabel4.setText("Evidence        :");
+        jLabel4.setText("Evidence  details      :");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(120, 190, 100, 50);
+        jLabel4.setBounds(90, 180, 130, 50);
+
+        evi_info.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                evi_infoActionPerformed(evt);
+            }
+        });
+        jPanel1.add(evi_info);
+        evi_info.setBounds(240, 190, 340, 60);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,7 +119,7 @@ public class HomePage extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 640, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -114,21 +133,45 @@ public class HomePage extends javax.swing.JFrame {
         pack();
     }// </editor-fold>                        
 
-    private void VictimNameFieldActionPerformed(java.awt.event.ActionEvent evt) {                                                
+    private void evi_numActionPerformed(java.awt.event.ActionEvent evt) {                                        
         // TODO add your handling code here:
-    }                                               
+        evi_no = Integer.parseInt(evi_num.getText());
+        System.out.println("took inp");
+    }                                       
 
     private void AddEvidenceActionPerformed(java.awt.event.ActionEvent evt) {                                            
         // TODO add your handling code here:
+        System.out.println("reached here");
+        for(int i=0;i<nc.blockchain.size();i++)
+        {
+            prev_hash = nc.blockchain.get(i).previousHash;
+        }
+        boolean val=true;
+        nc.createBlock(evi_no,nc.currentUser,loc,prev_hash,evi_det);        
+            output.setText("evidence added successfully");        
     }                                           
 
     private void MineBlockButtonActionPerformed(java.awt.event.ActionEvent evt) {                                                
         // TODO add your handling code here:
+        mined_val = nc.blockchain.get(0).mineBlock(4);
     }                                               
 
     private void viewUserButtonActionPerformed(java.awt.event.ActionEvent evt) {                                               
         // TODO add your handling code here:
+        nc.viewUser();
     }                                              
+
+    private void SubmissionLocActionPerformed(java.awt.event.ActionEvent evt) {                                              
+        // TODO add your handling code here:
+        loc=SubmissionLoc.getText();
+        System.out.println("took inp");
+    }                                             
+
+    private void evi_infoActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // TODO add your handling code here:
+        evi_det = evi_info.getText();
+        System.out.println("took inp");
+    }                                        
 
     /**
      * @param args the command line arguments
@@ -167,16 +210,21 @@ public class HomePage extends javax.swing.JFrame {
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton AddEvidence;
-    private javax.swing.JTextArea EvidenceArea;
     private javax.swing.JButton MineBlockButton;
-    private javax.swing.JTextField SuspectNameField;
-    private javax.swing.JTextField VictimNameField;
+    private javax.swing.JTextField SubmissionLoc;
+    private javax.swing.JTextField evi_info;
+    private javax.swing.JTextField evi_num;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel output;
     private javax.swing.JButton viewUserButton;
     // End of variables declaration                   
+    NoobChain nc;
+    String loc;
+    String evi_det;
+    int evi_no;
+    String prev_hash;
+    String mined_val;
 }
